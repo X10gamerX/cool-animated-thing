09ed392be5bcad5a6cf7cb7b060b2131b5388cc3
X10gamerX <127437994+X10gamerX@users.noreply.github.com>
2023-03-21 13:49:21 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,43 +1,45 @@
 input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . # . #
+            . . . . .
+            . . . . .
+            `)
+    }
     basic.showLeds(`
         . . . . .
-        . . . . .
-        . . . . .
+        # . . . .
+        . . # . #
         . . . . .
         . . . . .
         `)
     basic.showLeds(`
         . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
+        . . # . .
+        # . . . #
         . . . . .
         . . . . .
         `)
     basic.showLeds(`
         . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
+        . . . . #
+        # . # . .
         . . . . .
         . . . . .
         `)
